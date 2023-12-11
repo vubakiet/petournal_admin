e16d365b04357e7379813dd8a5cea9580e488d55
edwardwilliam <justinbk1412@gmail.com>
2023-12-12 05:08:30 +0700

    update

--- next.config.js
+++ next.config.js
@@ -4,8 +4,8 @@ const nextConfig = {
         appDir: true,
     },
     env: {
-        NEXT_APP_API_URL: "https://petournal.onrender.com/api/v1",
-        // NEXT_APP_API_URL: "http://localhost:5001/api/v1",
+        // NEXT_APP_API_URL: "https://petournal.onrender.com/api/v1",
+        NEXT_APP_API_URL: "http://localhost:5001/api/v1",
     },
     images: {
         domains: ["firebasestorage.googleapis.com"],
